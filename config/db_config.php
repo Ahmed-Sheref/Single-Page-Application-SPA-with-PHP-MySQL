1eb91b17a34c1f9ac571ef34b978fdd6f728940d
AhmeSheref <ahwy837@gmail.com>
2026-04-22 00:56:18 +0200

    Generate Database connection

---
+++ config/db_config.php
@@ -0,0 +1,15 @@
+<?php
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$database = "book_finder";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
